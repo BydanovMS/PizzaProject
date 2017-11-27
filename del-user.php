dc965a5d34898f806dd2d6b29cb0aac292828948
BydanovMS <bydanovms@gmail.com>
2017-11-27 09:51:17 +0000

    123

--- del-user.php
+++ del-user.php
@@ -1,15 +1,16 @@
 <?php
-echo "Пользователь $email удален!";
+echo "Пользователь $email удален2!";
 $pdo = include("DB.php");
 
 $email = htmlspecialchars($_POST["email"]);
 $nickname = htmlspecialchars($_POST["nickname"]);
 
-$sql = "delete users where $email in (?)";
+//$sql = "insert into users values(?, ?)";
+ $sql = "delete users where $email in (?)";
 
 var_dump($sql);
 
 $statement = $pdo->prepare($sql);
-// $statement->execute([$email, $nickname]);
+$statement->execute([$email, $nickname]);
 
 echo "Пользователь $email удален!";
--- main.js
+++ main.js
@@ -1,3 +1,26 @@
+$(function() {
+    var btn = $('#new-user-add-btn'); //del
+    var email = $('#email');
+    var nickname = $('#nickname');
+    
+    btn.click(function(e) {
+        e.preventDefault();
+        
+        $.ajax({
+            method: "POST",
+            url: "add-user.php", //del-user.php
+            data: {
+                email: email.val(),
+                nickname: nickname.val()
+            }
+        }).done(function( msg ) {
+            alert( "Result: " + msg );
+            location.reload();
+        })
+    });
+}
+)
+
 // $(function() {
 //     var btn = $('#new-user-add-btn');
 //     var email = $('#email');
@@ -20,26 +43,3 @@
 //     });
 // }
 // )
-
-$(function() {
-    var btn = $('#user-del-btn');
-    var email = $('#email');
-    var nickname = $('#nickname');
-    
-    btn.click(function(e) {
-        e.preventDefault();
-        
-        $.ajax({
-            method: "POST",
-            url: "add-user.php",
-            data: {
-                email: email.val(),
-                nickname: nickname.val()
-            }
-        }).done(function( msg ) {
-            alert( "Result: " + msg );
-            location.reload();
-        })
-    });
-}
-)
